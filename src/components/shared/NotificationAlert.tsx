4ce5171c8b59065c63c79545145e53ec2cc07630
webRemember <urlwebremember@gmail.com>
2025-04-09 12:01:52 -0500

    feat: add various shared components including CircleProgress, Cursor, DownloadCVButton, MouseAnimation, NotificationAlert, ParallaxImage, ParallaxRotateImg, and SkillCard with associated types and functionality

---
+++ src/components/shared/NotificationAlert.tsx
@@ -0,0 +1,102 @@
+"use client";
+
+import { type FC, useState, useEffect } from "react";
+import { Card, CardContent } from "@/components/ui/card";
+import { AlertCircle, CheckCircle, Info, XCircle } from "lucide-react";
+
+type NotificationType = "success" | "error" | "warning" | "info";
+
+interface NotificationAlertProps {
+  type: NotificationType;
+  title: string;
+  message: string;
+  duration?: number; // Duration in seconds
+  onClose?: () => void;
+}
+
+const NotificationAlert: FC<NotificationAlertProps> = ({
+  type,
+  title,
+  message,
+  duration = 5, // Default duration of 5 seconds
+  onClose,
+}) => {
+  const [timeLeft, setTimeLeft] = useState(duration);
+  const [isVisible, setIsVisible] = useState(true);
+
+  useEffect(() => {
+    if (timeLeft > 0) {
+      const timer = setTimeout(() => {
+        setTimeLeft(timeLeft - 0.1);
+      }, 100);
+      return () => clearTimeout(timer);
+    } else {
+      setIsVisible(false);
+      if (onClose) onClose();
+    }
+  }, [timeLeft, onClose]);
+
+  const getTypeStyles = (type: NotificationType) => {
+    switch (type) {
+      case "success":
+        return "bg-green-50 border-green-500 text-green-700";
+      case "error":
+        return "bg-red-50 border-red-500 text-red-700";
+      case "warning":
+        return "bg-yellow-50 border-yellow-500 text-yellow-700";
+      case "info":
+        return "bg-blue-50 border-blue-500 text-blue-700";
+    }
+  };
+
+  const getIcon = (type: NotificationType) => {
+    switch (type) {
+      case "success":
+        return <CheckCircle className="h-6 w-6 text-green-500" />;
+      case "error":
+        return <XCircle className="h-6 w-6 text-red-500" />;
+      case "warning":
+        return <AlertCircle className="h-6 w-6 text-yellow-500" />;
+      case "info":
+        return <Info className="h-6 w-6 text-blue-500" />;
+    }
+  };
+
+  const getProgressColor = (type: NotificationType) => {
+    switch (type) {
+      case "success":
+        return "bg-green-500";
+      case "error":
+        return "bg-red-500";
+      case "warning":
+        return "bg-yellow-500";
+      case "info":
+        return "bg-blue-500";
+    }
+  };
+
+  if (!isVisible) return null;
+
+  return (
+    <Card className={`${getTypeStyles(type)} relative overflow-hidden mt-4`}>
+      <CardContent className="flex items-center p-3">
+        <div className="shrink-0">{getIcon(type)}</div>
+        <div className="ml-3">
+          <h3 className="text-sm font-medium">{title}</h3>
+          <div className="mt-2 text-sm">
+            <p>{message}</p>
+          </div>
+        </div>
+      </CardContent>
+      <div
+        className={`absolute bottom-0 left-0 h-1 ${getProgressColor(type)}`}
+        style={{
+          width: `${(timeLeft / duration) * 100}%`,
+          transition: "width 0.1s linear",
+        }}
+      />
+    </Card>
+  );
+};
+
+export default NotificationAlert;
---
+++ src/components/shared/SkillsList.tsx
@@ -0,0 +1,143 @@
+import { Skill } from "@/types/skills";
+import { Cloud, Code, Database, Server } from "lucide-react";
+import {
+  SiAmazonwebservices,
+  SiDocker,
+  SiGithub,
+  SiGooglecloud,
+  SiGraphql,
+  SiJavascript,
+  SiJest,
+  SiMongodb,
+  SiNextdotjs,
+  SiNodedotjs,
+  SiPython,
+  SiReact,
+  SiRedux,
+  SiTypescript,
+} from "react-icons/si";
+
+type CategoryId = "all" | "frontend" | "backend" | "devops" | "cloud";
+
+export interface SkillWithCategory extends Skill {
+  category: string;
+}
+
+export const getSkills = (activeCategory: CategoryId): SkillWithCategory[] => {
+  const skills: Record<Exclude<CategoryId, "all">, Skill[]> = {
+    frontend: [
+      {
+        name: "React.js",
+        icon: <SiReact className="text-[#61DAFB]" />,
+        level: 95,
+      },
+      {
+        name: "Next.js",
+        icon: <SiNextdotjs className="text-black" />,
+        level: 90,
+      },
+      {
+        name: "TypeScript",
+        icon: <SiTypescript className="text-[#3178C6]" />,
+        level: 90,
+      },
+      {
+        name: "JavaScript",
+        icon: <SiJavascript className="text-[#F7DF1E]" />,
+        level: 95,
+      },
+      {
+        name: "GraphQL",
+        icon: <SiGraphql className="text-[#E10098]" />,
+        level: 75,
+      },
+      {
+        name: "Redux Toolkit",
+        icon: <SiRedux className="text-[#764ABC]" />,
+        level: 85,
+      },
+    ],
+    backend: [
+      {
+        name: "Node.js",
+        icon: <SiNodedotjs className="text-[#339933]" />,
+        level: 85,
+      },
+      {
+        name: "SQL Server",
+        icon: <Database className="text-[#CC2927]" />,
+        level: 70,
+      },
+      {
+        name: "MongoDB",
+        icon: <SiMongodb className="text-[#47A248]" />,
+        level: 75,
+      },
+      { name: ".NET 8", icon: <Code className="text-[#512BD4]" />, level: 40 },
+      {
+        name: "Python",
+        icon: <SiPython className="text-[#3776AB]" />,
+        level: 60,
+      },
+    ],
+    devops: [
+      {
+        name: "Docker",
+        icon: <SiDocker className="text-[#2496ED]" />,
+        level: 65,
+      },
+      { name: "GitHub", icon: <SiGithub className="text-black" />, level: 80 },
+      { name: "CI/CD", icon: <SiGithub className="text-black" />, level: 65 },
+      { name: "Jest", icon: <SiJest className="text-[#C21325]" />, level: 80 },
+      {
+        name: "React Testing Library",
+        icon: <Code className="text-[#E33332]" />,
+        level: 77,
+      },
+    ],
+    cloud: [
+      {
+        name: "AWS",
+        icon: <SiAmazonwebservices className="text-[#232F3E]" />,
+        level: 30,
+      },
+      {
+        name: "GCP",
+        icon: <SiGooglecloud className="text-[#4285F4]" />,
+        level: 30,
+      },
+      { name: "Azure", icon: <Cloud className="text-[#0078D4]" />, level: 30 },
+      { name: "IIS", icon: <Server className="text-[#5E5E5E]" />, level: 80 },
+    ],
+  };
+
+  // Si la categoría es "all", mostramos todas las categorías
+  if (activeCategory === "all") {
+    return Object.entries(skills).flatMap(([category, skillList]) =>
+      skillList.map((skill) => ({
+        ...skill,
+        category:
+          category === "frontend"
+            ? "Frontend"
+            : category === "backend"
+            ? "Backend"
+            : category === "devops"
+            ? "DevOps & Testing"
+            : "Cloud",
+      }))
+    );
+  }
+
+  // Si hay una categoría activa, retornamos solo los skills de esa categoría
+  return skills[activeCategory].map((skill) => ({
+    ...skill,
+    category:
+      activeCategory === "frontend"
+        ? "Frontend"
+        : activeCategory === "backend"
+        ? "Backend"
+        : activeCategory === "devops"
+        ? "DevOps & Testing"
+        : "Cloud",
+  }));
+};
